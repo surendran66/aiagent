74912f44dfc8391467c14e3aca0dee8daabfabb7
surendran66 <surendran66@gmail.com>
2026-02-02 14:00:53 -0600

    n8n automated build: 2026-02-02T20:00:53.185Z

---
+++ 23000testing/src/test/java/com/example/23000testing/HelloWorldControllerTest.java
@@ -0,0 +1,23 @@
+package com.example.23000testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@WebMvcTest(HelloWorldController.class)
+class HelloWorldControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void helloReturnsHelloWorld() throws Exception {
+        mockMvc.perform(get("/"))
+               .andExpect(status().isOk())
+               .andExpect(content().string("Hello, World!"));
+    }
+}
