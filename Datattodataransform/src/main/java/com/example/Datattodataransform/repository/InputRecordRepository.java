d4b5cf61580c210e63b48c28b26a5cdc472d2cf2
surendran66 <surendran66@gmail.com>
2026-02-02 09:53:54 -0600

    Delete Datattodataransform directory

--- Datattodataransform/src/main/java/com/example/Datattodataransform/repository/InputRecordRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Datattodataransform.repository;
-
-import com.azure.spring.data.cosmos.repository.CosmosRepository;
-import com.example.Datattodataransform.model.InputRecord;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InputRecordRepository extends CosmosRepository<InputRecord, String> {
-}
