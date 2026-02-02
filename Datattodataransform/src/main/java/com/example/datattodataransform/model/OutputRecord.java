bfc4280a52dd27a2d0a11ce7b2cb08709aff7127
surendran66 <surendran66@gmail.com>
2026-02-02 09:13:31 -0600

    n8n automated build: 2026-02-02T15:13:31.307Z

---
+++ Datattodataransform/src/main/java/com/example/datattodataransform/model/OutputRecord.java
@@ -0,0 +1,25 @@
+package com.example.datattodataransform.model;
+
+import com.azure.spring.data.cosmos.core.mapping.Container;
+import com.azure.spring.data.cosmos.core.mapping.PartitionKey;
+import lombok.Data;
+
+@Data
+@Container(containerName = "outputdata")
+public class OutputRecord {
+    @PartitionKey
+    private String recordId;
+    private String id;
+    private String service;
+    private Sla sla = new Sla();
+    private String receivedTime;
+    private String completedTime;
+    private long durationMinutes;
+    private boolean breached;
+    private String owner;
+
+    @Data
+    public static class Sla {
+        private String state;
+    }
+}
