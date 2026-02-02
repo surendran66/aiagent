4f72057e851e3a96df5a72ceeff75d32e8afb230
surendran66 <surendran66@gmail.com>
2026-02-02 11:41:16 -0600

    n8n automated build: 2026-02-02T17:41:16.122Z

---
+++ Health API/src/main/java/com/example/healthapi/HealthApiApplication.java
@@ -0,0 +1,11 @@
+package com.example.healthapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HealthApiApplication.class, args);
+    }
+}
