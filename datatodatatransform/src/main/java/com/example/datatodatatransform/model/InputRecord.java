f861e99cfc70b2c3da77d0b38c5b6ef846586bba
surendran66 <surendran66@gmail.com>
2026-02-02 09:59:23 -0600

    n8n automated build: 2026-02-02T15:59:22.685Z

---
+++ datatodatatransform/src/main/java/com/example/datatodatatransform/model/InputRecord.java
@@ -0,0 +1,25 @@
+package com.example.datatodatatransform.model;
+
+import com.azure.spring.data.cosmos.core.mapping.Container;
+import com.azure.spring.data.cosmos.core.mapping.PartitionKey;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Container(containerName = "inputdata")
+public class InputRecord {
+    private String id;
+    private String system;
+    private String status;
+    private String user;
+    private String receivedTime;
+    private String completedTime;
+
+    @PartitionKey
+    public String getId() {
+        return id;
+    }
+}
