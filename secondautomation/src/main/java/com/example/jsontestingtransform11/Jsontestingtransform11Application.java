81af2b29c0395f286c55c34a7578bae2eb9ee00f
surendran66 <surendran66@gmail.com>
2026-02-02 17:33:57 -0600

    n8n automated build: 2026-02-02T23:33:56.879Z

---
+++ secondautomation/src/main/java/com/example/jsontestingtransform11/Jsontestingtransform11Application.java
@@ -0,0 +1,11 @@
+package com.example.jsontestingtransform11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jsontestingtransform11Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Jsontestingtransform11Application.class, args);
+    }
+}
