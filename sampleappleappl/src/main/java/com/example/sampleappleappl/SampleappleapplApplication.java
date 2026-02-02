f99993076b7c66ab585d5ef61863625ab81cb52c
surendran66 <surendran66@gmail.com>
2026-02-02 11:29:55 -0600

    n8n automated build: 2026-02-02T17:29:54.635Z

---
+++ sampleappleappl/src/main/java/com/example/sampleappleappl/SampleappleapplApplication.java
@@ -0,0 +1,11 @@
+package com.example.sampleappleappl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleappleapplApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SampleappleapplApplication.class, args);
+    }
+}
