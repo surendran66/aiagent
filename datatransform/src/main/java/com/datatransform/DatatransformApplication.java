a304e55bc4a4747d3fad701001652b2296cbaa06
surendran66 <surendran66@gmail.com>
2026-02-02 00:55:54 -0600

    n8n automated build: 2026-02-02T06:55:53.511Z

---
+++ datatransform/src/main/java/com/datatransform/DatatransformApplication.java
@@ -0,0 +1,11 @@
+package com.datatransform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatatransformApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DatatransformApplication.class, args);
+    }
+}
