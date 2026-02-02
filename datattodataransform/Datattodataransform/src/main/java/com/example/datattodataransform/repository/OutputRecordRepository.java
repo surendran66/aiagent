5c38b479425ec28abd5dbdb672bdd58ae3d82a70
surendran66 <surendran66@gmail.com>
2026-02-02 10:50:08 -0600

    Delete datattodataransform/Datattodataransform directory

--- datattodataransform/Datattodataransform/src/main/java/com/example/datattodataransform/repository/OutputRecordRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.datattodataransform.repository;
-
-import com.azure.spring.data.cosmos.repository.CosmosRepository;
-import com.example.datattodataransform.model.OutputRecord;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OutputRecordRepository extends CosmosRepository<OutputRecord, String> {
-}
