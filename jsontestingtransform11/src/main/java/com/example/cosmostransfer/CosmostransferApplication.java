937cca97d69c2f87ecb20d935d730be54d0363a6
surendran66 <surendran66@gmail.com>
2026-02-02 17:05:24 -0600

    n8n automated build: 2026-02-02T23:05:23.309Z

---
+++ jsontestingtransform11/src/main/java/com/example/cosmostransfer/CosmostransferApplication.java
@@ -0,0 +1,11 @@
+package com.example.cosmostransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmostransferApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CosmostransferApplication.class, args);
+    }
+}
