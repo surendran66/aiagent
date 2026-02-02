9d722edbc5d54c5ecd9e7d96ed274f066329d8f8
surendran66 <surendran66@gmail.com>
2026-02-02 01:22:39 -0600

    n8n automated build: 2026-02-02T07:22:39.185Z

---
+++ cosmos-transfer/n8nhello/src/main/java/com/example/n8nhello/N8nhelloApplication.java
@@ -0,0 +1,12 @@
+package com.example.n8nhello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class N8nhelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(N8nhelloApplication.class, args);
+    }
+}
