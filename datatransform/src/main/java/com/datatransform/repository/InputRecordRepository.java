0e1c064884d259c6a693e1c83454b6aec86e3c30
surendran66 <surendran66@gmail.com>
2026-02-02 10:50:40 -0600

    Delete datatransform directory

--- datatransform/src/main/java/com/datatransform/repository/InputRecordRepository.java
+++
@@ -1,9 +0,0 @@
-package com.datatransform.repository;
-
-import com.datatransform.model.InputRecord;
-import com.azure.spring.data.cosmos.repository.CosmosRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InputRecordRepository extends CosmosRepository<InputRecord, String> {
-}
--- datatransform/src/main/java/com/datatransform/repository/OutputRecordRepository.java
+++
@@ -1,9 +0,0 @@
-package com.datatransform.repository;
-
-import com.datatransform.model.OutputRecord;
-import com.azure.spring.data.cosmos.repository.CosmosRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OutputRecordRepository extends CosmosRepository<OutputRecord, String> {
-}
