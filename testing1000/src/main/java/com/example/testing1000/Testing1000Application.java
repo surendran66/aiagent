bb043d0c0648cb7d87f12e252de74acab3659b86
surendran66 <surendran66@gmail.com>
2026-02-02 12:27:25 -0600

    n8n automated build: 2026-02-02T18:27:24.461Z

---
+++ testing1000/src/main/java/com/example/testing1000/Testing1000Application.java
@@ -0,0 +1,11 @@
+package com.example.testing1000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testing1000Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Testing1000Application.class, args);
+    }
+}
