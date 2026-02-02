9b954ed9382b9c01d14772749a162bce49a2a8ac
surendran66 <surendran66@gmail.com>
2026-02-02 12:00:34 -0600

    n8n automated build: 2026-02-02T18:00:34.296Z

---
+++ lak/src/main/java/com/example/lak/LakApplication.java
@@ -0,0 +1,13 @@
+package com.example.lak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LakApplication {
+
+    public static void main(String[] args) {
+        System.out.println("hello World !");
+        SpringApplication.run(LakApplication.class, args);
+    }
+}
