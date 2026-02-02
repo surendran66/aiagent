35023d9fa2fd7c12ff74e24e97b66be5df35a842
surendran66 <surendran66@gmail.com>
2026-02-02 17:31:54 -0600

    n8n automated build: 2026-02-02T23:31:53.748Z

---
+++ firstautomation/src/main/java/com/firstautomation/repository/InputRecordRepository.java
@@ -0,0 +1,7 @@
+package com.firstautomation.repository;
+
+import com.firstautomation.model.InputRecord;
+import org.springframework.data.repository.CrudRepository;
+
+public interface InputRecordRepository extends CrudRepository<InputRecord, Long> {
+}
