19689238d1bbde5c6ab86c2e3bc9c222b580ebbf
surendran66 <surendran66@gmail.com>
2026-02-02 09:30:44 -0600

    n8n automated build: 2026-02-02T15:30:43.573Z

---
+++ default-app/Datattodataransform/src/main/java/com/datattodataransform/model/InputRecord.java
@@ -0,0 +1,17 @@
+package com.datattodataransform.model;
+
+import com.azure.spring.data.cosmos.core.mapping.Container;
+import com.azure.spring.data.cosmos.core.mapping.PartitionKey;
+import lombok.Data;
+
+@Data
+@Container(containerName = "inputdata")
+public class InputRecord {
+    @PartitionKey
+    private String id;
+    private String system;
+    private String status;
+    private String user;
+    private String receivedTime;
+    private String completedTime;
+}
