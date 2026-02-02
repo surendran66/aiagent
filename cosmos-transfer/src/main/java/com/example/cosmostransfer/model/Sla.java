e688c2e2ff7d310ff0314c8f251a3f575c4d24f2
surendran66 <surendran66@gmail.com>
2026-02-02 00:19:46 -0600

    n8n automated build: 2026-02-02T06:19:46.180Z

---
+++ cosmos-transfer/src/main/java/com/example/cosmostransfer/model/Sla.java
@@ -0,0 +1,12 @@
+package com.example.cosmostransfer.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Sla {
+    private String state;
+}
