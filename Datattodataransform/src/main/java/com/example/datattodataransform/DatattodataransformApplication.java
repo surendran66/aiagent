d09186480040f25601f78dda3d2b9746100ca0b7
surendran66 <surendran66@gmail.com>
2026-02-02 09:13:27 -0600

    n8n automated build: 2026-02-02T15:13:27.484Z

---
+++ Datattodataransform/src/main/java/com/example/datattodataransform/DatattodataransformApplication.java
@@ -0,0 +1,11 @@
+package com.example.datattodataransform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatattodataransformApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DatattodataransformApplication.class, args);
+    }
+}
