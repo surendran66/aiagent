1b229727ff22019c452e67188049d8eae56c595d
surendran66 <surendran66@gmail.com>
2026-02-02 09:59:22 -0600

    n8n automated build: 2026-02-02T15:59:21.870Z

---
+++ datatodatatransform/src/main/java/com/example/datatodatatransform/DatatodatatransformApplication.java
@@ -0,0 +1,10 @@
+package com.example.datatodatatransform;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatatodatatransformApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DatatodatatransformApplication.class, args);
+    }
+}
