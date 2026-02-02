11203371a01b55bd34561e5053c4c1bdb5a21f9f
surendran66 <surendran66@gmail.com>
2026-02-02 11:10:45 -0600

    Delete spring-boot-starter-parent directory

--- spring-boot-starter-parent/src/main/java/com/example/springcosmosapp/repository/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springcosmosapp.repository;
-
-import com.example.springcosmosapp.model.Item;
-import com.azure.spring.data.cosmos.repository.CosmosRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemRepository extends CosmosRepository<Item, String> {
-}
