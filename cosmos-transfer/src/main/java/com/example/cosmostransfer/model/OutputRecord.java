e305b44b0d0bb0d2a2f7e3ba50c57d6423502468
surendran66 <surendran66@gmail.com>
2026-02-02 00:19:47 -0600

    n8n automated build: 2026-02-02T06:19:46.973Z

---
+++ cosmos-transfer/src/main/java/com/example/cosmostransfer/model/OutputRecord.java
@@ -0,0 +1,24 @@
+package com.example.cosmostransfer.model;
+
+import com.azure.spring.data.cosmos.core.mapping.Container;
+import com.azure.spring.data.cosmos.core.mapping.PartitionKey;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Container(containerName = "outputdata")
+public class OutputRecord {
+    @PartitionKey
+    private String recordId;
+    private String id;
+    private String service;
+    private Sla sla;
+    private String receivedTime;
+    private String completedTime;
+    private long durationMinutes;
+    private boolean breached;
+    private String owner;
+}
