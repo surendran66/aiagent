8fbc8ba1e4288e8a5e42f188bee9fb3820425f4d
surendran66 <surendran66@gmail.com>
2026-02-02 11:36:28 -0600

    n8n automated build: 2026-02-02T17:36:28.423Z

---
+++ jsontojsontaransformsuren/src/main/java/com/suren/jsontojsontaransformsuren/JsontojsontaransformsurenApplication.java
@@ -0,0 +1,11 @@
+package com.suren.jsontojsontaransformsuren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsontojsontaransformsurenApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JsontojsontaransformsurenApplication.class, args);
+    }
+}
