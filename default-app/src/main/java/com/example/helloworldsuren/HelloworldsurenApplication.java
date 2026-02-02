46edbbbf1161e982b495e83612c537c3ed5e7f48
surendran66 <surendran66@gmail.com>
2026-02-01 22:17:34 -0600

    n8n automated build: 2026-02-02T04:17:34.455Z

---
+++ default-app/src/main/java/com/example/helloworldsuren/HelloworldsurenApplication.java
@@ -0,0 +1,11 @@
+package com.example.helloworldsuren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldsurenApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloworldsurenApplication.class, args);
+    }
+}
