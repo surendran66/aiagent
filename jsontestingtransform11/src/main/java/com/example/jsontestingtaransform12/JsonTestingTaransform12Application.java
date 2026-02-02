6ef9a542c6cf671a76909d72ec954464cf0ff41a
surendran66 <surendran66@gmail.com>
2026-02-02 17:05:13 -0600

    n8n automated build: 2026-02-02T23:05:13.045Z

---
+++ jsontestingtransform11/src/main/java/com/example/jsontestingtaransform12/JsonTestingTaransform12Application.java
@@ -0,0 +1,11 @@
+package com.example.jsontestingtaransform12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonTestingTaransform12Application {
+    public static void main(String[] args) {
+        SpringApplication.run(JsonTestingTaransform12Application.class, args);
+    }
+}
