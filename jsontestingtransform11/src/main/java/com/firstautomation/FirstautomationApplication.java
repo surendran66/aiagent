80d14fd458d0baa1da3393997f429cadacc25b13
surendran66 <surendran66@gmail.com>
2026-02-02 17:21:51 -0600

    n8n automated build: 2026-02-02T23:21:50.125Z

---
+++ jsontestingtransform11/src/main/java/com/firstautomation/FirstautomationApplication.java
@@ -0,0 +1,11 @@
+package com.firstautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstautomationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FirstautomationApplication.class, args);
+    }
+}
