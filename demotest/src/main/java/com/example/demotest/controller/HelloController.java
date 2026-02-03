b578dd37a662c18396fa87cce575c6e8f8d2e51a
surendran66 <surendran66@gmail.com>
2026-02-03 10:03:42 -0600

    n8n automated build: 2026-02-03T16:03:41.796Z

---
+++ demotest/src/main/java/com/example/demotest/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.demotest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
