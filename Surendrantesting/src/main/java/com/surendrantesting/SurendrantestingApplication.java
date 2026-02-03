3c43037f80cc7fb046f44d421372a600e397207d
surendran66 <surendran66@gmail.com>
2026-02-02 19:15:29 -0600

    n8n automated build: 2026-02-03T01:15:28.060Z

---
+++ Surendrantesting/src/main/java/com/surendrantesting/SurendrantestingApplication.java
@@ -0,0 +1,9 @@
+package com.surendrantesting;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class SurendrantestingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SurendrantestingApplication.class, args);
+    }
+}
