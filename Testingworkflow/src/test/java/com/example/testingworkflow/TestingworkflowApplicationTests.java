fe3fac0fc91d22c29cb3bacc9480f7df1ddf1030
surendran66 <surendran66@gmail.com>
2026-02-02 19:21:11 -0600

    n8n automated build: 2026-02-03T01:21:10.195Z

---
+++ Testingworkflow/src/test/java/com/example/testingworkflow/TestingworkflowApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.testingworkflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingworkflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
