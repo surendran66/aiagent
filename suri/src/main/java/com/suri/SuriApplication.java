31ff4ca1e5c9ad75914b6afe404b47a316114193
surendran66 <surendran66@gmail.com>
2026-02-02 19:08:07 -0600

    n8n automated build: 2026-02-03T01:08:06.023Z

---
+++ suri/src/main/java/com/suri/SuriApplication.java
@@ -0,0 +1,11 @@
+package com.suri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuriApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SuriApplication.class, args);
+    }
+}
