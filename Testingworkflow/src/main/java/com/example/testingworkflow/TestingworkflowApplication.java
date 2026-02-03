e2af908a2c0f565810f3a7abde9f45adce1b06d1
surendran66 <surendran66@gmail.com>
2026-02-02 19:21:01 -0600

    n8n automated build: 2026-02-03T01:20:59.726Z

---
+++ Testingworkflow/src/main/java/com/example/testingworkflow/TestingworkflowApplication.java
@@ -0,0 +1,11 @@
+package com.example.testingworkflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingworkflowApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestingworkflowApplication.class, args);
+    }
+}
