234fa0989504a085894310fa5005ea3145a69989
surendran66 <surendran66@gmail.com>
2026-02-02 19:21:08 -0600

    n8n automated build: 2026-02-03T01:21:06.488Z

---
+++ Testingworkflow/src/main/java/com/example/testingworkflow/repository/OutputRecordRepository.java
@@ -0,0 +1,7 @@
+package com.example.testingworkflow.repository;
+
+import com.example.testingworkflow.model.OutputRecord;
+
+public interface OutputRecordRepository {
+    void save(OutputRecord record);
+}
