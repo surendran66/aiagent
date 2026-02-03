4247fe6ca32af4469cda8d02af7a22737cfa66e9
surendran66 <surendran66@gmail.com>
2026-02-02 19:15:34 -0600

    n8n automated build: 2026-02-03T01:15:33.445Z

---
+++ Surendrantesting/src/main/java/com/surendrantesting/repository/OutputRecordRepository.java
@@ -0,0 +1,5 @@
+package com.surendrantesting.repository;
+import com.surendrantesting.model.OutputRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface OutputRecordRepository extends JpaRepository<OutputRecord, Long> {
+}
