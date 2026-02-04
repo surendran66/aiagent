7ade753104a7b3437b234483eb3f4a688b9f0926
surendran66 <surendran66@gmail.com>
2026-02-04 12:10:19 -0600

    AI Build commit

--- jsontransformation/src/main/java/com/example/jsontransformation/model/InputRecord.java
+++ jsontransformation/src/main/java/com/example/jsontransformation/model/InputRecord.java
@@ -1,37 +1,65 @@
 package com.example.jsontransformation.model;
 
 import com.azure.spring.data.cosmos.core.mapping.Container;
-import com.azure.spring.data.cosmos.core.mapping.PartitionKey;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceConstructor;
 
-@Container(containerName = "inputRecords")
+@Container(containerName = "InputRecord")
 public class InputRecord {
     @Id
     private String id;
-
-    @PartitionKey
-    private String data;
+    private String name;
+    private String type;
+    private String status;
+    private String priority;
+    private String sla;
 
     public InputRecord() {}
 
-    public InputRecord(String id, String data) {
+    @PersistenceConstructor
+    public InputRecord(String id, String name, String type, String status, String priority, String sla) {
         this.id = id;
-        this.data = data;
+        this.name = name;
+        this.type = type;
+        this.status = status;
+        this.priority = priority;
+        this.sla = sla;
     }
 
     public String getId() {
         return id;
     }
-
     public void setId(String id) {
         this.id = id;
     }
-
-    public String getData() {
-        return data;
+    public String getName() {
+        return name;
     }
-
-    public void setData(String data) {
-        this.data = data;
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public String getPriority() {
+        return priority;
+    }
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+    public String getSla() {
+        return sla;
+    }
+    public void setSla(String sla) {
+        this.sla = sla;
     }
 }
