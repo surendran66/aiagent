6709ffcb60a592769669710ca55ce98529984a47
surendran66 <surendran66@gmail.com>
2026-02-04 11:43:54 -0600

    AI Build commit

---
+++ jsontransformation/src/main/java/com/example/jsontransformation/model/InputRecord.java
@@ -0,0 +1,37 @@
+package com.example.jsontransformation.model;
+
+import com.azure.spring.data.cosmos.core.mapping.Container;
+import com.azure.spring.data.cosmos.core.mapping.PartitionKey;
+import org.springframework.data.annotation.Id;
+
+@Container(containerName = "inputRecords")
+public class InputRecord {
+    @Id
+    private String id;
+
+    @PartitionKey
+    private String data;
+
+    public InputRecord() {}
+
+    public InputRecord(String id, String data) {
+        this.id = id;
+        this.data = data;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
