216eb42fcacf99886ede9730ae8266fb91734f52
surendran66 <surendran66@gmail.com>
2026-02-04 12:10:14 -0600

    AI Build commit

--- jsontransformation/src/main/java/com/example/jsontransformation/JsonTransformationApplication.java
+++ jsontransformation/src/main/java/com/example/jsontransformation/JsonTransformationApplication.java
@@ -2,10 +2,9 @@ package com.example.jsontransformation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import com.azure.spring.data.cosmos.repository.config.EnableCosmosRepositories;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 @EnableCosmosRepositories(basePackages = "com.example.jsontransformation.repository")
 public class JsonTransformationApplication {
     public static void main(String[] args) {
