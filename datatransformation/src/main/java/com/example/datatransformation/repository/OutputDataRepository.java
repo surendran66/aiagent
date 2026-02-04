af55330a113485a9c8b35be0ed4b7fe75e67b688
surendran66 <surendran66@gmail.com>
2026-02-04 12:49:49 -0600

    AI Build commit

---
+++ datatransformation/src/main/java/com/example/datatransformation/repository/OutputDataRepository.java
@@ -0,0 +1,9 @@
+package com.example.datatransformation.repository;
+
+import com.example.datatransformation.model.Outputdata;
+import com.azure.spring.data.cosmos.repository.CosmosRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OutputDataRepository extends CosmosRepository<Outputdata, String> {
+}
