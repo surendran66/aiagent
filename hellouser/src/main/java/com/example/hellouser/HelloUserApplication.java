a4c32de3d7005109aa8abb1d2e35601a09f0e8b1
surendran66 <surendran66@gmail.com>
2026-02-04 10:53:59 -0600

    AI Build commit

---
+++ hellouser/src/main/java/com/example/hellouser/HelloUserApplication.java
@@ -0,0 +1,11 @@
+package com.example.hellouser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloUserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloUserApplication.class, args);
+    }
+}
