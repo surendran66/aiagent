fd4f40e709ba8bae5b8b43fb06537cbdf5c01869
surendran66 <surendran66@gmail.com>
2026-02-05 10:02:48 -0600

    AI Build commit

--- datatojsontransformation/src/main/java/com/example/datatojsontransformation/model/OutputRecord.java
+++ datatojsontransformation/src/main/java/com/example/datatojsontransformation/model/OutputRecord.java
@@ -4,58 +4,27 @@ import com.azure.spring.data.cosmos.core.mapping.Container;
 import com.azure.spring.data.cosmos.core.mapping.PartitionKey;
 import org.springframework.data.annotation.Id;
 
-@Container(containerName = "outputdata")
+@Container(containerName = "outputRecords")
 public class OutputRecord {
     @Id
     private String id;
+
     @PartitionKey
-    private String recordId;
-    private String service;
-    private Sla sla;
-    private String receivedTime;
-    private String completedTime;
-    private double durationMinutes;
-    private boolean breached;
-    private String Owner;
+    private String data;
 
-    public OutputRecord() {}
-
-    public OutputRecord(String id, String recordId, String service, Sla sla, String receivedTime, String completedTime, double durationMinutes, boolean breached, String Owner) {
-        this.id = id;
-        this.recordId = recordId;
-        this.service = service;
-        this.sla = sla;
-        this.receivedTime = receivedTime;
-        this.completedTime = completedTime;
-        this.durationMinutes = durationMinutes;
-        this.breached = breached;
-        this.Owner = Owner;
+    public String getId() {
+        return id;
     }
 
-    public String getId() { return id; }
-    public void setId(String id) { this.id = id; }
-    public String getRecordId() { return recordId; }
-    public void setRecordId(String recordId) { this.recordId = recordId; }
-    public String getService() { return service; }
-    public void setService(String service) { this.service = service; }
-    public Sla getSla() { return sla; }
-    public void setSla(Sla sla) { this.sla = sla; }
-    public String getReceivedTime() { return receivedTime; }
-    public void setReceivedTime(String receivedTime) { this.receivedTime = receivedTime; }
-    public String getCompletedTime() { return completedTime; }
-    public void setCompletedTime(String completedTime) { this.completedTime = completedTime; }
-    public double getDurationMinutes() { return durationMinutes; }
-    public void setDurationMinutes(double durationMinutes) { this.durationMinutes = durationMinutes; }
-    public boolean isBreached() { return breached; }
-    public void setBreached(boolean breached) { this.breached = breached; }
-    public String getOwner() { return Owner; }
-    public void setOwner(String Owner) { this.Owner = Owner; }
+    public void setId(String id) {
+        this.id = id;
+    }
 
-    public static class Sla {
-        private String state;
-        public Sla() {}
-        public Sla(String state) { this.state = state; }
-        public String getState() { return state; }
-        public void setState(String state) { this.state = state; }
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
     }
 }
