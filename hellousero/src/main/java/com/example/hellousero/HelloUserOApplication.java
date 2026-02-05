7a62a223a039d823605a327da17e40a145177f32
surendran66 <surendran66@gmail.com>
2026-02-05 08:28:07 -0600

    n8n automated build: 2026-02-05T14:28:07.525Z

---
+++ hellousero/src/main/java/com/example/hellousero/HelloUserOApplication.java
@@ -0,0 +1,12 @@
+package com.example.hellousero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloUserOApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloUserOApplication.class, args);
+    }
+}
