fc09b003e5f1087cea2e17c96991cf7112ed68a5
surendran66 <surendran66@gmail.com>
2026-02-04 20:43:47 -0600

    n8n automated build: 2026-02-05T02:43:48.294Z

---
+++ DemoSusaitest/src/main/java/com/example/DemoSusaitest/DemoSusaitestApplication.java
@@ -0,0 +1,11 @@
+package com.example.DemoSusaitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSusaitestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSusaitestApplication.class, args);
+    }
+}
