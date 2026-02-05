f70f77b5ff67fccb90835573b27db2d4f9def61c
surendran66 <surendran66@gmail.com>
2026-02-05 09:46:14 -0600

    AI Build commit

---
+++ hellouseroptum/src/main/java/com/optum/hellouseroptum/HelloUserOptumApplication.java
@@ -0,0 +1,11 @@
+package com.optum.hellouseroptum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloUserOptumApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloUserOptumApplication.class, args);
+    }
+}
