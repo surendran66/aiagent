6a79451679fab07d9cf3e88ed6e698e317a917ea
surendran66 <surendran66@gmail.com>
2026-02-05 10:02:57 -0600

    AI Build commit

---
+++ datatojsontransformation/src/main/java/com/example/datatojsontransformation/repository/OutputRecordRepository.java
@@ -0,0 +1,9 @@
+package com.example.datatojsontransformation.repository;
+
+import com.example.datatojsontransformation.model.OutputRecord;
+import com.azure.spring.data.cosmos.repository.CosmosRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OutputRecordRepository extends CosmosRepository<OutputRecord, String> {
+}
