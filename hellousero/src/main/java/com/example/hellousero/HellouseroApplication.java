abfc4deab18bcd029d482429d395f8d4c8f6d766
surendran66 <surendran66@gmail.com>
2026-02-05 08:28:03 -0600

    n8n automated build: 2026-02-05T14:28:03.718Z

---
+++ hellousero/src/main/java/com/example/hellousero/HellouseroApplication.java
@@ -0,0 +1,20 @@
+package com.example.hellousero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HellouseroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellouseroApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, Usero!";
+    }
+}
