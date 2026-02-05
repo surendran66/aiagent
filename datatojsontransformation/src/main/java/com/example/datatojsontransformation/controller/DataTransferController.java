6abb92b086a7b1fa4ce63243a49d645087b15429
surendran66 <surendran66@gmail.com>
2026-02-05 10:02:43 -0600

    AI Build commit

--- datatojsontransformation/src/main/java/com/example/datatojsontransformation/controller/DataTransferController.java
+++ datatojsontransformation/src/main/java/com/example/datatojsontransformation/controller/DataTransferController.java
@@ -1,14 +1,12 @@
 package com.example.datatojsontransformation.controller;
 
-import com.example.datatojsontransformation.model.OutputRecord;
 import com.example.datatojsontransformation.service.DataTransferService;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import java.util.List;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/transfer")
+@RequestMapping("/data-transfer")
 public class DataTransferController {
     private final DataTransferService dataTransferService;
 
@@ -16,8 +14,8 @@ public class DataTransferController {
         this.dataTransferService = dataTransferService;
     }
 
-    @PostMapping("/start")
-    public List<OutputRecord> startTransfer() {
-        return dataTransferService.transferAndTransform();
+    @PostMapping("/transfer")
+    public void transferData() {
+        dataTransferService.transferData();
     }
 }
